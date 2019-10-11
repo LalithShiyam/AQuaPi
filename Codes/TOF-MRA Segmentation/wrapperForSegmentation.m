22d7a12a167e6c6e1e4bce60ab0a563a4b1f11dd
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2019-10-11 14:33:36 +0200

    Mod: WrapperForSegmentation.m

--- Codes/TOF-MRA Segmentation/wrapperForSegmentation.m
+++ Codes/TOF-MRA Segmentation/wrapperForSegmentation.m
@@ -16,12 +16,38 @@
 % point in time, but will be added in due course.
 
 %% Program start
-
 % Copy your physical path of the 3D time-of-flight MR angiography DICOM
 % images.
 
 coreSegInputs.pathOfAngio = ''; % To be filled in by the user!
 
-% Running the coreSegmentation algorithm
+%% Hard-coded variables.
 
-coreSegmentation(coreSegInputs);
+% Error messages
+
+errorMsg{1}='This program needs a path to the "DICOM" series!';
+errorMsg{2}='Raise an issue in github...';
+
+% path to store the data.
+
+cd(coreSegInputs.pathOfAngio); 
+cd .. 
+coreSegInputs.path2StoreSeg=pwd; % path where the output will be stored.
+
+
+%% Preliminary checks are being done here, to see if the program can be run.
+fileFormat=checkFileFormat(coreSegInputs.pathOfAngio); % check if the folder has dicom images.
+switch fileFormat 
+    case 'DICOM'
+        disp(['DICOM images found in ',coreSegInputs.pathOfAngio,'...']);
+        disp('Applying segmentation algorithm on the dataset...');
+        coreSegmentation(coreSegInputs); % Running the coreSegmentation algorithm
+        
+    otherwise
+        error(errorMsg{1});
+        error(errorMsg{2});
+end
+
+%%
+
+
