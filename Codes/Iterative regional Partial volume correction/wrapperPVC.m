7ccc810cccd29c9beb5fed27c0060c8e5ef50bc9
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2019-10-13 00:02:56 +0200

    update.

--- Codes/Iterative regional Partial volume correction/wrapperPVC.m
+++ Codes/Iterative regional Partial volume correction/wrapperPVC.m
@@ -18,13 +18,15 @@
     tLower2=datetime(2016,06,25); tUpper2=datetime(2016,10,13)
     tLower3=datetime(2016,10,24); tUpper3=datetime(2017,1,12)
     tLower4=datetime(2017,01,23); tUpper4=datetime(2017,04,25)
-
+    
+    %---------------------------------------------------------------------%
     % For every new block of cross-calibration interval, the bottom code
     % needs to be appended in the following syntax:
     
     % if isbetween(datevector,tLowerX,tUpperX)
     %  PVC.crossCalibrationFactor=X.XXX
     % end
+    %---------------------------------------------------------------------%
     
     if isbetween(dateVector,tLower1,tUpper1)
        PVC.crossCalibrationFactor=1.141
@@ -41,37 +43,9 @@
 
 %% Hard-coded variables   
 
-    PVC.psfFWHM= 5
-    PVC.P2Bratio=1.06 % plasma to blood ratio
+    PVC.psfFWHM= 5 % in mm
+    PVC.P2Bratio=1.06 % plasma to blood ratio based on 20 volunteer analysis.
     PVC.petMoCo=false; % MR based MoCo only.
     PVC.AC='CT'; % Type of attenuation map - optional
-    = [pathSuffix,subjectID{lp},'/Processed data'];
-    % to comment out
-   % r=[]; 
-    % to comment out
-    [pvcOutputs] = tempPVC2(PVC,r);
-%     if strcmp(acMaps,'RESOLUTE')
-%         resoluteAUC1(lp)=pvcOutputs.rAUC1;
-%         resoluteAUC2(lp)=pvcOutputs.rAUC2;
-%         resoluteAUC(lp)=pvcOutputs.rAUC;
-%     else
-%         if strcmp(acMaps,'PCT')
-%             PCTauc1(lp)=pvcOutputs.rAUC1;
-%             PCTauc2(lp)=pvcOutputs.rAUC2;
-%             PCTauc(lp)=pvcOutputs.rAUC;
-%         end
-%     end
-    %rawArtery{lp}(ilp)=pvcOutputs.arteryTACarea;
-%     rawAUC{lp}(ilp)= pvcOutputs.rawAUC;
-end
-end
-% resoluteAUC1=resoluteAUC1';
-% resoluteAUC2=resoluteAUC2';
-% resoluteAUC=resoluteAUC';
-% PCTauc=PCTauc';
-% PCTauc1=PCTauc1';
-% PCTauc2=PCTauc2';
-% T=table(subjectID,resoluteAUC,resoluteAUC1,resoluteAUC1,resoluteAUC2,PCTauc,PCTauc1,PCTauc2);
-% cd(whereToStoreResults)
-% writetable(T);
-toc
+    [pvcOutputs] = coreIterativePVC(PVC,r);
+
